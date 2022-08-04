32db5e4b159d4bb166a6443facbf0a0d3c301e57
Shazma Siddiqui <siddiquishaz7@gmail.com>
2022-08-04 12:41:08 +0530

    phpcs code formatting

--- plugins/system/schema/schema.php
+++ plugins/system/schema/schema.php
@@ -5,9 +5,9 @@
  *
  * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
 
-defined('_JEXEC') or die;
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+ */
 
 use Joomla\CMS\Application\CMSApplicationInterface;
 use Joomla\CMS\Form\FormHelper;
@@ -24,144 +24,143 @@ use Joomla\CMS\Plugin\PluginHelper;
  */
 class PlgSystemSchema extends CMSPlugin implements SubscriberInterface
 {
-	/**
-	 * @var    \Joomla\Database\DatabaseDriver
-	 */
-	protected $db;
+    /**
+     * @var    \Joomla\Database\DatabaseDriver
+     *
+     */
+    protected $db;
 
-	/**
-	 * Load the language file on instantiation.
-	 *
-	 * @var    boolean
-	 * @since  4.0.0
-	 */
-	protected $autoloadLanguage = true;
+    /**
+     * Load the language file on instantiation.
+     *
+     * @var    boolean
+     * @since  4.0.0
+     */
+    protected $autoloadLanguage = true;
 
-	/**
-	 * Loads the CMS Application for direct access
-	 *
-	 * @var   CMSApplicationInterface
-	 * @since 4.0.0
-	 */
-	protected $app;
+    /**
+     * Loads the CMS Application for direct access
+     *
+     * @var   CMSApplicationInterface
+     * @since 4.0.0
+     */
+    protected $app;
 
-	/**
-	 * The name of the supported name to check against
-	 *
-	 * @var   string
-	 * @since 4.0.0
-	 */
-	protected $supportFunctionality = 'core.state';
+    /**
+     * The name of the supported name to check against
+     *
+     * @var   string
+     * @since 4.0.0
+     */
+    protected $supportFunctionality = 'core.state';
 
-	/**
-	 * Returns an array of events this subscriber will listen to.
-	 *
-	 * @return  array
-	 *
-	 * @since   4.0.0
-	 */
-	public static function getSubscribedEvents(): array
-	{
-		return [
-			'onBeforeCompileHead'                  => 'onBeforeCompileHead',
-			'onContentPrepareData'            => 'onContentPrepareData',
-			'onContentPrepareForm'            => 'onContentPrepareForm',
-			'onContentAfterSave'            => 'onContentAfterSave',
-		];
-	}
+    /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   4.0.0
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onBeforeCompileHead'                  => 'onBeforeCompileHead',
+            'onContentPrepareData'            => 'onContentPrepareData',
+            'onContentPrepareForm'            => 'onContentPrepareForm',
+            'onContentBeforeSave'            => 'onContentBeforeSave',
+        ];
+    }
 
-	/**
-	 * Runs on content preparation
-	 *
-	 * @param   EventInterface  $event  The event
-	 *
-	 * @since   4.0.0
-	 */
-	public function onContentPrepareData(EventInterface $event)
-	{
-		$context = $event->getArgument('0');
-		$data = $event->getArgument('1');
+    /**
+     * Runs on content preparation
+     *
+     * @param   EventInterface  $event  The event
+     *
+     * @since   4.0.0
+     *
+     */
+    public function onContentPrepareData(EventInterface $event)
+    {
+        $context = $event->getArgument('0');
+        $data = $event->getArgument('1');
 
-		// Check if we are manipulating a valid form.
-		if (!in_array($context, ['com_content.article']))
-		{
-			return true;
-		}
+        // Check if we are manipulating a valid form.
+        if (!in_array($context, ['com_content.article'])) {
+            return true;
+        }
 
-		return true;
+        return true;
+    }
 
-	}
+    /**
+     * The form event.
+     *
+     * @param   EventInterface  $event  The event
+     *
+     * @since   4.0.0
+     */
+    public function onContentPrepareForm(EventInterface $event)
+    {
+        $form = $event->getArgument('0');
 
-	/**
-	 * The form event.
-	 *
-	 * @param   EventInterface  $event  The event
-	 *
-	 * @since   4.0.0
-	 */
-	public function onContentPrepareForm(EventInterface $event)
-	{
-		$form = $event->getArgument('0');
+        // Check if we are manipulating a valid form
+        $context = $form->getName();
 
-		// Check if we are manipulating a valid form
-		$context = $form->getName();
+        if (!in_array($context, ['com_content.article'])) {
+            return true;
+        }
 
-		if (!in_array($context, ['com_content.article']))
-		{
-			return true;
-		}
+        //Load the form fields
+        FormHelper::addFormPath(__DIR__ . '/forms');
+        $form->loadFile('schema');
 
-		//Load the form fields
-		FormHelper::addFormPath(__DIR__ . '/forms');
-		$form->loadFile('schema');
+        $event   = AbstractEvent::create(
+            'onSchemaPrepareForm',
+            [
+                'subject' => $form,
+            ]
+        );
 
-		$event   = AbstractEvent::create(
-			'onSchemaPrepareForm',
-			[
-				'subject' => $form,
-			]
-		);
+        PluginHelper::importPlugin('schemaorg');
+        $this->app->getDispatcher()->dispatch('onSchemaPrepareForm', $event);
 
-		PluginHelper::importPlugin('schemaorg');
-		$this->app->getDispatcher()->dispatch('onSchemaPrepareForm', $event);
-
-		return true;
-	}
+        return true;
+    }
 
 /**
-	 * Saves form field data in the database
-	 *
-	 * @param   EventInterface $event
-	 *
-	 * @return  boolean
-	 */
-	public function onContentAfterSave(EventInterface $event)
-	{
-		$context = $event->getArgument('0');
+     * Saves form field data in the database
+     *
+     * @param   EventInterface $event
+     *
+     * @return  boolean
+     *
+     */
+    public function onContentBeforeSave(EventInterface $event)
+    {
+        $context = $event->getArgument('0');
 
-		if (!in_array($context, ['com_content.article']))
-		{
-			return true;
-		}
+        if (!in_array($context, ['com_content.article'])) {
+            return true;
+        }
 
-		$article = $event->getArgument('1');
-		$isNew = $event->getArgument('2');
-		$data = $event->getArgument('3');
+        $article = $event->getArgument('1');
+        $isNew = $event->getArgument('2');
+        $data = $event->getArgument('3');
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * This event is triggered before the framework creates the Head section of the Document
-	 *
-	 * @since   4.0.0
-	 */
-	public function onBeforeCompileHead()
-	{
-		$context = $this->app;
-		if (!$this->app->isClient('Site') && $context === 'com_content.article') {
-			return;
-		}
-	}
 
+    /**
+     * This event is triggered before the framework creates the Head section of the Document
+     *
+     * @since   4.0.0
+     */
+    public function onBeforeCompileHead()
+    {
+        $context = $this->app;
+        if (!$this->app->isClient('Site') && $context === 'com_content.article') {
+            return;
+        }
+    }
 }
