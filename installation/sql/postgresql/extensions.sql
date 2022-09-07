cfff800943b5e5e965db70a073c8621c2f3db75e
Shazma Siddiqui <siddiquishaz7@gmail.com>
2022-09-08 04:20:03 +0530

    Auto installs plugins

--- installation/sql/postgresql/extensions.sql
+++ installation/sql/postgresql/extensions.sql
@@ -893,6 +893,22 @@ CREATE INDEX "#__scheduler_tasks_idx_checked_out" ON "#__scheduler_tasks" ("chec
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `#__schemaorg`
+--
+
+CREATE TABLE IF NOT EXISTS `#__schemaorg` (
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`itemId` int,
+	`context` varchar(100),
+	`schemaType` varchar(100),
+	`schemaForm` text,
+	`schema` text,
+	PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+-- --------------------------------------------------------
+
+--
 -- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
 --   from contrib folder.
 -- This function comes from https://wiki.postgresql.org/wiki/Soundex
